2d1b9dc9abedb7b5cde27d569485e8da87769e64
Kert619 <108245652+Kert619@users.noreply.github.com>
2023-11-20 20:31:17 +0800

    update tracking

--- dockerfiles/nuxt.dockerfile
+++ dockerfiles/nuxt.dockerfile
@@ -2,7 +2,7 @@ FROM node:21.1.0-slim
 
 WORKDIR /app
 
-COPY src/.npmrc src/package-lock.json src/package.json ./
+COPY src/package.json .
 
 RUN npm install
 
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="flex h-screen">
     <!-- SIDEBAR -->
     <div
+      v-if="!isStudent"
       class="bg-primary-500 h-full w-64 transition-[width] duration-500 overflow-y-auto overflow-x-hidden shrink-0"
       :class="{ '!w-16': !expanded }"
     >
@@ -23,6 +24,9 @@
 
 <script setup>
 const expanded = ref(true);
+const authStore = useAuthStore();
+
+const isStudent = authStore.isStudent;
 
 function toggleExpand(value) {
   expanded.value = !value;
